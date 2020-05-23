5d85b5e0504abc155256495ed69f16d690acd2e9
defor <deforworks@gmail.com>
2020-05-24 02:09:38 +0700

    Подправил роуты

--- src/routes.php
+++ src/routes.php
@@ -2,8 +2,8 @@
 
 return [
     "~^$~" => [\MyProject\Controllers\MainController::class, 'main'], # www/
-    "~^articles/(\d+)$~" => [\MyProject\Controllers\ArticlesController::class, 'view'], # www/articles/1
-    "~^articles/(\d+)/edit$~" => [\MyProject\Controllers\ArticlesController::class, 'edit'], # www/articles/1/edit
-    "~^articles/add$~" => [\MyProject\Controllers\ArticlesController::class, 'add'], # www/articles/add
-    "~^articles/(\d+)/delete~" => [\MyProject\Controllers\ArticlesController::class, 'remove'], # www/articles/1/delete
+    "~^articles/(\d+)/?$~" => [\MyProject\Controllers\ArticlesController::class, 'view'], # www/articles/1/
+    "~^articles/(\d+)/edit/?$~" => [\MyProject\Controllers\ArticlesController::class, 'edit'], # www/articles/1/edit/
+    "~^add/?$~" => [\MyProject\Controllers\ArticlesController::class, 'add'], # www/articles/add/
+    "~^articles/(\d+)/delete/?~" => [\MyProject\Controllers\ArticlesController::class, 'remove'], # www/articles/1/delete/
 ];
