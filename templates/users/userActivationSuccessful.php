44ac50579930c51d73f6cf07d6d75871b748dae4
defor <deforworks@gmail.com>
2020-05-26 19:50:30 +0700

    Я тупой, забыл убрать TODO

--- templates/users/userActivationSuccessful.php
+++ templates/users/userActivationSuccessful.php
@@ -1,6 +1,6 @@
 <?php include __DIR__ . '/../header.php'; ?>
     <div style="text-align: center;">
         <h1>Активация прошла успешно!</h1>
-        <a href="www/login">Войти в аккаунт</a> <!-- TODO: Здесь будет ссылка для входа в аккаунт -->
+        <a href="www/login">Войти в аккаунт</a>
     </div>
 <?php include __DIR__ . '/../footer.php'; ?>
