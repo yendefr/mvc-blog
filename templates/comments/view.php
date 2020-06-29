4ed8e47a87efbc6ff360c118e4aeace8ef66687f
defor <deforworks@gmail.com>
2020-06-29 14:28:45 +0300

    Bug fix

--- templates/comments/view.php
+++ templates/comments/view.php
@@ -2,7 +2,7 @@
   <?php foreach ($comments as $comment): ?>
     <tr>
       <td colspan="2" rowspan="1">
-        <h3><?= $user->getNickname() ?></h3> <h4><?= $comment->getCreatedAt() ?></h4>
+        <h3><?= $comment->getAuthor()->getNickname() ?></h3> <h4><?= $comment->getCreatedAt() ?></h4>
         <p><?= $comment->getText() ?></p>
       </td>
     </tr>
