5f1ae89d64d2b58235c8342f1e2bd6d416ee1784
defor <deforworks@gmail.com>
2020-07-18 01:34:33 +0300

    Страница статьи

--- templates/articles/view.php
+++ templates/articles/view.php
@@ -12,24 +12,25 @@
                   <span><?= $article->getText() ?></span>
                 </div>
 
-                <div class="article__info">
-                  <div class="article__item">
-                    <span>Автор: </span><a href=""><?= $article->getAuthor()->getNickname() ?></a>
+                <div class="article__bar">
+                  <div class="article__info">
+                    <div class="article__item">
+                      <span>Автор: </span><a href=""><?= $article->getAuthor()->getNickname() ?></a>
+                    </div>
+                    <div class="article__item">
+                      <span><?= $article->getCreatedAt() ?></span>
+                    </div>
                   </div>
-                  <div class="article__item">
-                    <span><?= $article->getCreatedAt() ?></span>
+
+                  <div class="article__buttons">
+                    <div class="article__item">
+                      <a href="/delete"><button>Удалить</button></a>
+                    </div>
+                    <div class="article__item">
+                      <a href="/edit"><button>Редактировать</button></a>
+                    </div>
                   </div>
                 </div>
-
-                <div class="article__buttons">
-                  <div class="article__item">
-                    <a href="/delete"><button>Удалить</button></a>
-                  </div>
-                  <div class="article__item">
-                    <a href="/edit"><button>Редактировать</button></a>
-                  </div>
-                </div>
-
                 <!-- Добавить кнопки удаления/редактирования итд -->
 
             </div>
