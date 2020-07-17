313108c755bccf914e2a4170c72b42b56abdaaf2
defor <deforworks@gmail.com>
2020-07-18 01:27:53 +0300

    Страница статьи

--- templates/articles/view.php
+++ templates/articles/view.php
@@ -11,7 +11,7 @@
                 <div class="view-article__text">
                   <span><?= $article->getText() ?></span>
                 </div>
-                
+
                 <div class="article__info">
                   <div class="article__item">
                     <span>Автор: </span><a href=""><?= $article->getAuthor()->getNickname() ?></a>
@@ -20,6 +20,17 @@
                     <span><?= $article->getCreatedAt() ?></span>
                   </div>
                 </div>
+
+                <div class="article__buttons">
+                  <div class="article__item">
+                    <a href="/delete"><button>Удалить</button></a>
+                  </div>
+                  <div class="article__item">
+                    <a href="/edit"><button>Редактировать</button></a>
+                  </div>
+                </div>
+
+                <!-- Добавить кнопки удаления/редактирования итд -->
 
             </div>
         </div>
