ee4a15bb73556d42e748d5e23873ca3e66f6d91e
defor <deforworks@gmail.com>
2020-07-17 19:04:05 +0300

    Страница статьи

--- templates/articles/view.php
+++ templates/articles/view.php
@@ -1,21 +1,16 @@
-<?php include __DIR__ . '/../header.php'; ?>
-  <h1><?= $article->getName() ?></h1>
-  <p><?= $article->getText() ?></p>
+<?php include __DIR__ . '/../newHeader.php'; ?>
 
-  <p>Автор: <b><?php
-          if ($article->getAuthor() !== null) {
-            #TODO: Сделать профили пользователей и ссылки на них
-            echo "<a href='user/'>".$article->getAuthor()->getNickname()."</a>";
-          } else {
-            echo "Пользователь удалён";
-          }
-          ?></b></p>
-  <p>Дата создания: <b><?= $article->getCreatedAt() ?></b></p>
+<main>
+    <section class="view-article">
+        <div class="wrapper">
+            <div class="view-article__box">
+                <div class="view-article__title">
+                    <h1><?= $article->getName() ?></h1>
+                </div>
 
-  <?php if ($article->getAuthor() !== null) { ?>
-    <?php if ($article->getAuthor()->getId() == $user->getId()) { ?>
-      <a href="<?= $article->getId() ?>/edit">Редактировать</a> <br>
-      <a href="<?= $article->getId() ?>/delete">Удалить</a>
-    <?php } ?>
-  <?php } ?>
-<?php include __DIR__ . '/../footer.php'; ?>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php include __DIR__ . '/../newFooter.php'; ?>
