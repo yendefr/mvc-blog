0c643c4d9023bacf600877f005f900fab623a660
defor <deforworks@gmail.com>
2020-07-17 20:08:58 +0300

    Страница статьи

--- templates/articles/view.php
+++ templates/articles/view.php
@@ -7,8 +7,18 @@
                 <div class="view-article__title">
                     <h1><?= $article->getName() ?></h1>
                 </div>
+
                 <div class="view-article__text">
                   <span><?= $article->getText() ?></span>
+                </div>
+                
+                <div class="article__info">
+                  <div class="article__item">
+                    <span>Автор: </span><a href=""><?= $article->getAuthor()->getNickname() ?></a>
+                  </div>
+                  <div class="article__item">
+                    <span><?= $article->getCreatedAt() ?></span>
+                  </div>
                 </div>
 
             </div>
